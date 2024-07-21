9c6143e52931c0708a195cdbeedc189b0b1b2f3f
dedicatedcloud <98156514+dedicatedcloud@users.noreply.github.com>
2024-07-21 13:06:10 +0100

    Create dw.sh

---
+++ dw.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wget https://burst.shopifycdn.com/business_ideas/womens-fashion-business-idea.zip
+wget https://burst.shopifycdn.com/business_ideas/Art-supplies-starter-kit.zip
+wget https://burst.shopifycdn.com/business_ideas/makeup-business-idea-starter-kit.zip
+wget https://burst.shopifycdn.com/business_ideas/camping-gear-business-starter-kit.zip
+wget https://burst.shopifycdn.com/business_ideas/mens-fashion-starter-kit.zip
+wget https://burst.shopifycdn.com/business_ideas/t-shirt-business-starter-kit.zip
+wget  https://burst.shopifycdn.com/business_ideas/kitchen-supplies-business-starter-kit.zip
+wget https://burst.shopifycdn.com/business_ideas/sock-business-idea-starter-kit.zip
+wget https://burst.shopifycdn.com/business_ideas/fitness-tracker-business-starter-kit.zip
+wget https://burst.shopifycdn.com/business_ideas/headphones-business-starter-kit.zip
+wget https://burst.shopifycdn.com/business_ideas/tea-business-starter-kit.zip
+wget https://burst.shopifycdn.com/business_ideas/cell-phone-cases.zip
+wget https://burst.shopifycdn.com/business_ideas/coffee.zip
+
